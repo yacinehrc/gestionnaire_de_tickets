a58d4c1f4dda9c4504ee92f6c525128839d264f1
Yacine <pro.h.yacine@gmail.com>
2026-04-12 14:17:05 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ date_default_timezone_set('Europe/Paris');
 /**
  * PARAMÈTRES DE CONNEXION
  */
-$host = 'sql113.infinityfree.com';
-$dbname = 'if0_41051738_atelier_des_jeux';
-$user = 'if0_41051738';
-$pass = '64DdaZ06j2C2kf'; 
+$host = '';
+$dbname = '';
+$user = '';
+$pass = ''; 
 
 try {
     // Connexion PDO
